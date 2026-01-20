6b2226bf121f18b7c08005a7be52bf7f155c01ef
Marko2401 <marko.mijic@bulme.at>
2026-01-20 13:59:32 +0100

    Ende 20.01

--- willkleidung/urls.py
+++ willkleidung/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("market.urls")),  # Verbindet unsere App
+    path('admin/', admin.site.urls),
+    path('', include('market.urls')),
 ]
+
+# WICHTIG: Diese Zeilen hinzufügen!
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root='static')
